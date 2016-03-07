dfb92e8ed171a024dcd37ba524c5b7f8392aa46b
gerhardpilcher <gerhard.pilcher@gmail.com>
2016-03-07 16:37:28 -0500

    Graded homework

--- Rollins_HW6_Part2.R
+++ Rollins_HW6_Part2.R
@@ -54,4 +54,59 @@ final #this is the result we want
 pearson_var<-as.matrix(a$pearson) #putting the pearson coefficent data as a matrix so we can plot
 plot(pearson_var, main="Pearson Correlation Coefficents", col="blue") #plotting coefficents, adding title and changing color of scatter dots
 
+##Prof G: You got the first part and began correctly on the second part. The third part is to create
+##Prof G: a scatterplot for each unique pair (expcluding a column paried with itself). You started
+##Prof G: to put the column names correctly in part 2 but only created one pair name.
+##Prof G: See my code below:
+
+####################################################
+# Function to generate R-square values and correlation
+# values. All R-square values are returned in one dataframe.
+# Only correlation (R) values that exceed the parameter 
+# threshold are returned in a separate dataframe.
+# The function also creates labels by combining the
+# names of the columns pairs into a single string
+# like "Carat-Price"
+####################################################
+get_rsq_corr <- function(Num_Frame, threshold) {
+   #Create correlation matrix using cor function
+   #Specify pearson correlation
+   corr_a <- cor(Num_Frame, method="pearson")
+   #Set up a threshold and null vectors before entering loop
+   r_square <- NULL
+   rsq_names <- NULL
+   corr_list <- NULL
+   corr_names <- NULL
+   
+   #Get the length of the one dimension of the square matrix
+   len <- length(corr_a[1,])
+   
+   #Only loop through the upper right triangle
+   for (i in (1:(len-1))) {
+      for (j in ((i+1):len)) {
+         #Form the name pair and add to the named pair vector
+         pair_name <- paste(names(corr_a[,1])[[i]],names(corr_a[1,])[[j]],sep="-")
+         rsq_names <- c(rsq_names, pair_name)
+         
+         #Add the r_square value to the value vector
+         r_square <- c(r_square, corr_a[i,j]^2)
+         
+         #if the threshold is exceeded, add the name and value to the
+         #respective correlation vectors
+         if (abs(corr_a[i,j]) > threshold) {
+            corr_names <- c(corr_names, pair_name)
+            corr_list <- c(corr_list, corr_a[i,j]) 
+         }
+      }
+   }
+   
+   #create the dataframes and label the columns
+   rsq_df <- data.frame(cbind(rsq_names, r_square))
+   names(rsq_df)[1] <- "Pair"
+   names(rsq_df)[2] <- "Value"
+   corr_df <- data.frame(cbind(corr_names, corr_list))
+   names(corr_df)[1] <- "Pair"
+   names(corr_df)[2] <- "Value"
+   return(list("rsquare"=rsq_df, "correlation"=corr_df))
+}
 
